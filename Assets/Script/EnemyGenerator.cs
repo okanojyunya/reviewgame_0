54eca711dd8617b7c0f93a4ebdf41d27d6a2ff69
okanojyunya <monogamingvantan@gmail.com>
2025-02-11 17:07:07 +0900

    敵自動生成処理実装

---
+++ Assets/Script/EnemyGenerator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 一定間隔で敵を生成するスクリプト
+/// </summary>
+public class EnemyGenerator : MonoBehaviour
+{
+    /// <summary>敵のプレハブ</summary>
+    [SerializeField] GameObject enemy;
+    /// <summary>敵を生成するまでのインターバル</summary>
+    [SerializeField] float interval;
+    /// <summary>経過時間</summary>
+    float time;
+
+    void Move()
+    {
+        time = Time.deltaTime;
+        if (time > interval)
+        {
+            time = 0;
+            Instantiate(enemy, gameObject.transform.position, Quaternion.identity);
+        }
+    }
+}
